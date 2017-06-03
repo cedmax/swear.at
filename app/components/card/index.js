378c91cea2177ec2256cc2d65d89d2fe3da3e442
Marco Cedaro <cedmax@users.noreply.github.com>
2017-06-04 00:08:46 +0100

    tweaks

--- app/components/card/index.js
+++ app/components/card/index.js
@@ -3,9 +3,11 @@ import Base from './base'
 
 export default connectStyles(Base, {
   card: {
+    'margin': 'auto',
     'text-align': 'center',
     background: '#DC4227',
     'max-width': '500px',
+    'width': '90%',
     padding: '2%',
     'box-shadow': '0 4px 8px 0 rgba(0,0,0,0.2)',
     'transition': '0.3s',
