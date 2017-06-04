573eb56d11595eb5d353e3540f77003bb93bb679
Marco Cedaro <cedmax@users.noreply.github.com>
2017-06-04 01:24:24 +0100

    added export map

--- next.config.js
+++ next.config.js
@@ -3,5 +3,10 @@ module.exports = {
     config.externals = config.externals || {}
     config.externals['styletron-server'] = 'styletron-server'
     return config
+  },
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" }
+    }
   }
 }
