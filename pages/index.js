2e96adf1125c3fb19503885e4ba7dd8c16ca7e59
Marco Cedaro <cedmax@users.noreply.github.com>
2017-06-04 01:40:45 +0100

    tentively fix routing

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,33 @@
 import StyletronProvider from 'providers/styletron'
 import ReduxProvider from 'providers/redux'
 import Main from 'components/main'
+import {confirmName, updateName, updateInsult} from 'store/actions'
+import capitalize from 'capitalize'
+import store from 'store'
 
-export default function (props) {
-  return (
-    <ReduxProvider>
-      <StyletronProvider>
-        <Main />
-      </StyletronProvider>
-    </ReduxProvider>
-  )
+export default class Index extends React.Component {
+  componentWillMount () {
+    if (typeof window!=='undefined') {
+      const pathname = window.location.pathname
+      const paths = pathname.split('/');
+      const name = capitalize(paths[1] || '')
+      store.dispatch(updateName(name))
+      store.dispatch(confirmName(name))
+
+      const insult = capitalize(paths[2] || '')
+      if (insult) {
+        store.dispatch(updateInsult(insult))
+      }
+    }
+  }
+
+  render () {
+    return (
+      <ReduxProvider>
+        <StyletronProvider>
+          <Main />
+        </StyletronProvider>
+      </ReduxProvider>
+    )
+  }
 }
