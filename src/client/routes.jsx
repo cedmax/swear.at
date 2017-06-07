20808cd2f2b20caae6cda7725cb9779b17df7fda
Marco Cedaro <cedmax@users.noreply.github.com>
2017-06-07 16:29:10 +0100

    redirect (not working)

--- src/client/routes.jsx
+++ src/client/routes.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import {Route, IndexRoute} from 'react-router'
 import Index from './pages/index'
 import Form from './pages/form'
+import obfuscator from './helpers/obfuscator'
 
 export const routes = (
   <Route path="/">
+    <Route path="share/:id" component={Form} onEnter={(state, replace) => {
+      replace(obfuscator.decode(state.params.id))
+    }}/>
     <Route path=":name">
       <Route path=":insult" component={Form} />
       <IndexRoute component={Form} />
