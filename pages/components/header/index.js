9057aaab39007d05ee30bdba67b56fc605ee5c28
Marco Cedaro <cedmax@users.noreply.github.com>
2017-06-07 21:52:06 +0100

    removed unused

--- pages/components/header/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Nav from 'components/nav';
-import classes from './index.css';
-
-export default () => (
-  <header className={classes.header}>
-    <Nav />
-  </header>
-)
