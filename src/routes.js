8a588c48cfc0597ef2b1eb39645a4a19c903f515
Marco Cedaro <cedmax@users.noreply.github.com>
2018-05-03 19:06:17 +0100

    prettier + fixed some glitches

--- src/routes.js
+++ src/routes.js
@@ -1,32 +1,40 @@
-import React from 'react'
-import {Route, IndexRoute} from 'react-router'
-import Index from './pages/index'
-import Form from './pages/form'
-import obfuscator from './helpers/obfuscator'
-import store from './store';
-import {updateName, updateInsult, confirmName} from './store/actions';
+import React from "react";
+import { Route, IndexRoute } from "react-router";
+import Index from "./pages/index";
+import Form from "./pages/form";
+import obfuscator from "./helpers/obfuscator";
+import store from "./store";
+import { updateName, updateInsult, confirmName } from "./store/actions";
 
-export const routes = (paths) => (
+export const routes = paths => (
   <Route path="/">
-    <Route path="share/:id" component={Form} onEnter={(state, replace) => {
-      if (typeof window !== 'undefined') {
-        replace(obfuscator.decode(state.params.id))
-      }
-    }}/>
-    
-    <Route path=":name" onEnter={(state) => {
-      if (paths[state.params.name]){
-        window.location.href = `/${state.params.name}`
-      } else {
-        store.dispatch(updateName(state.params.name))
-        store.dispatch(confirmName(state.params.name))
-        console.log(state.params.insult)
-        store.dispatch(updateInsult(state.params.insult || ''))
-      }
-    }}>
+    <Route
+      path="share/:id"
+      component={Form}
+      onEnter={(state, replace) => {
+        if (typeof window !== "undefined") {
+          replace(obfuscator.decode(state.params.id));
+        }
+      }}
+    />
+
+    <Route
+      path=":name"
+      onEnter={(state) => {
+        if (paths[state.params.name]) {
+          if (typeof window !== "undefined") {
+            window.location.href = `/${state.params.name}`;
+          }
+        } else {
+          store.dispatch(updateName(state.params.name));
+          store.dispatch(confirmName(state.params.name));
+          store.dispatch(updateInsult(state.params.insult || ""));
+        }
+      }}
+    >
       <Route path=":insult" component={Form} />
       <IndexRoute component={Form} />
     </Route>
     <IndexRoute component={Index} />
   </Route>
-)
+);
